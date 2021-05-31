01b69443590752d68aeaca2ee1b48a45262106e9
Manish Kumar <mm000143@gmail.com>
2021-05-31 08:00:16 +0530

    Update reader_writer.c

--- reader_writer.c
+++ reader_writer.c
@@ -1,17 +1,18 @@
 /*
 Sample Output:
+Reader: Read global value: 0. Readers: 5. Writers: 0.
+Reader: Read global value: 0. Readers: 4. Writers: 0.
+Reader: Read global value: 0. Readers: 3. Writers: 0.
+Reader: Read global value: 0. Readers: 2. Writers: 0.
+Reader: Read global value: 0. Readers: 1. Writers: 0.
 Writer: Written global value: 1. Readers: 0. Writers: 1.
 Writer: Written global value: 2. Readers: 0. Writers: 1.
 Writer: Written global value: 3. Readers: 0. Writers: 1.
-Reader: Read global value: 3. Readers: 5. Writers: 0.
-Reader: Read global value: 3. Readers: 4. Writers: 0.
-Reader: Read global value: 3. Readers: 3. Writers: 0.
-Reader: Read global value: 3. Readers: 2. Writers: 0.
-Reader: Read global value: 3. Readers: 1. Writers: 0.
 Writer: Written global value: 4. Readers: 0. Writers: 1.
 Writer: Written global value: 5. Readers: 0. Writers: 1.
 Parent(/main) quiting
 */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,10 +21,13 @@ Parent(/main) quiting
 
 #define NUM_WRITERS 5
 #define NUM_READERS 5
+#define NUM_READS 5
+#define NUM_WRITES 5
 
 int global_variable = 0;
 int num_writers = 0;
 int num_readers = 0;
+int waiting_readers = 0;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;  	/* mutex lock for shared variable */
 pthread_cond_t c_write = PTHREAD_COND_INITIALIZER; /* writers waits on this cond var */
@@ -70,40 +74,56 @@ int main(int argc, char *argv[]) {
 
 /* Write value(s) */
 void *writer_procedure(void *param) {
-    pthread_mutex_lock(&m);
-    while(num_readers>0 || num_writers>0){
-        pthread_cond_wait(&c_write, &m);
-    }
-    num_writers++;
-    pthread_mutex_unlock(&m);
-    sleep(1);
-    global_variable+=1;
-    printf("Writer: Written global value: %d. "
-            "Readers: %d. Writers: %d.\n",
-            global_variable, num_readers, num_writers);
-    pthread_mutex_lock(&m);
-    num_writers--;
-    pthread_mutex_unlock(&m);
-    pthread_cond_broadcast(&c_write);
-    pthread_cond_broadcast(&c_read);
+	for(int i=0; i<NUM_WRITES; i++){
+	    pthread_mutex_lock(&m);
+	    while(num_readers>0 || num_writers>0){
+	        pthread_cond_wait(&c_write, &m);
+	    }
+	    num_writers++;
+	    pthread_mutex_unlock(&m);
+	    usleep(1000 * (random() % NUM_READERS + NUM_WRITERS));	
+	    global_variable+=1;
+	    printf("Writer: Written global value: %d. "
+	            "Readers: %d. Writers: %d.\n",
+	            global_variable, num_readers, num_writers);
+	    pthread_mutex_lock(&m);
+	    num_writers--;
+	    // Prioritise readers as soon as writer is done. If
+	    // there is no reader, then trigger writers also.
+	    if(waiting_readers>0){
+	    	pthread_cond_broadcast(&c_read);
+	    }
+	    else {
+	    	pthread_cond_broadcast(&c_read);
+		pthread_cond_broadcast(&c_write);
+	    }
+	    pthread_mutex_unlock(&m);
+	}
     return 0;
 }
 
 /* Read value(s); Note the consumer never terminates */
 void *reader_procedure(void *param) {
-    pthread_mutex_lock(&m);
-    while(num_writers>0){
-        pthread_cond_wait(&c_read, &m);
-    }
-    num_readers++;
-    pthread_mutex_unlock(&m);
-    sleep(1);
-    printf("Reader: Read global value: %d. "
-            "Readers: %d. Writers: %d.\n",
-            global_variable, num_readers, num_writers);
-    pthread_mutex_lock(&m);
-    num_readers--;
-    pthread_mutex_unlock(&m);
-    pthread_cond_broadcast(&c_write);
+	for(int i=0; i<NUM_READS; i++){
+	    pthread_mutex_lock(&m);
+	    while(num_writers>0){
+		waiting_readers++;
+	        pthread_cond_wait(&c_read, &m);
+	    	waiting_readers--;
+	    }
+	    num_readers++;
+	    pthread_mutex_unlock(&m);
+	    
+		usleep(1000 * (random() % NUM_READERS + NUM_WRITERS));	
+	    printf("Reader: Read global value: %d. "
+	            "Readers: %d. Writers: %d.\n",
+	            global_variable, num_readers, num_writers);
+	    pthread_mutex_lock(&m);
+	    num_readers--;
+	    if(waiting_readers==0){
+	    	pthread_cond_broadcast(&c_write);
+	    }
+	    pthread_mutex_unlock(&m);
+	}
     return 0;
 }
